c4dde62b4a78a24d3fdba124b4d3c156882d00e5
2Jong1 <whddnjs5255@gmail.com>
2023-06-06 20:41:32 +0900

    Merge pull request #12 from vlqhel3440/main
    ID FInd, Login API, PasswordChange, Nickname Check

--- src/main/java/aily/server/controller/ImageController.java
+++ src/main/java/aily/server/controller/ImageController.java
@@ -15,19 +15,47 @@ import java.nio.file.Paths;
 @RestController
 public class ImageController {
 
+    private static final String IMAGE_DIRECTORY = "C:/Users/rnral/OneDrive/바탕 화면/스프링/image/";
+
     @GetMapping(value = "image/{userid}.png", produces = MediaType.IMAGE_JPEG_VALUE)
     public ResponseEntity<ByteArrayResource> getImage(@PathVariable("userid") String id) throws IOException {
-        String imagePath = "/home/lee/image/"+id+"/image.png";
+        String imagePath = IMAGE_DIRECTORY + id + "/image.png";
         Path path = Paths.get(imagePath);
 
-        byte[] imageBytes = Files.readAllBytes(path);
+        if (Files.exists(path)) {
+            byte[] imageBytes = Files.readAllBytes(path);
+            ByteArrayResource resource = new ByteArrayResource(imageBytes);
+            return ResponseEntity.ok()
+                    .contentLength(imageBytes.length)
+                    .contentType(MediaType.IMAGE_JPEG)
+                    .body(resource);
+        } else {
+            // 파일이 존재하지 않을 경우
+            // 요구에 맞게 파일 생성
+            createImageFile(id);
 
-        ByteArrayResource resource = new ByteArrayResource(imageBytes);
+            // 파일을 다시 읽어와 리턴
+            byte[] imageBytes = Files.readAllBytes(path);
+            ByteArrayResource resource = new ByteArrayResource(imageBytes);
+            return ResponseEntity.ok()
+                    .contentLength(imageBytes.length)
+                    .contentType(MediaType.IMAGE_JPEG)
+                    .body(resource);
+        }
+    }
 
-        return ResponseEntity.ok()
-                .contentLength(imageBytes.length)
-                .contentType(MediaType.IMAGE_JPEG)
-                .body(resource);
+    private void createImageFile(String id) throws IOException {
+        String directoryPath = IMAGE_DIRECTORY + id;
+
+        Path directory = Paths.get(directoryPath);
+        if (!Files.exists(directory)) {
+            Files.createDirectories(directory);
+        }
+
+        // 신규 파일 생성
+        Files.createFile(directory);
+
+        // 파일 생성 후에 필요한 초기화 작업 등을 수행할 수 있음
+        // ...
     }
 }
-
