23bf29b749163044b3a24e53ff79548d1c4d6730
NamJungHyeon <nam9490@gmail.com>
2023-06-06 14:38:29 +0900

    modify recycling dictionary classes and add BoardDTO

---
+++ src/main/java/aily/server/DTO/BoardDTO.java
@@ -0,0 +1,28 @@
+package aily.server.DTO;
+
+
+import aily.server.entity.redict;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class BoardDTO {
+    private Long id;
+    private int number;
+    private String title;
+    private String content;
+
+    public static BoardDTO toBoardDTO(redict board){
+        BoardDTO boardDTO = new BoardDTO();
+        boardDTO.setId(board.getId());
+        boardDTO.setNumber(board.getNumber());
+        boardDTO.setTitle(board.getTitle());
+        boardDTO.setContent(board.getContent());
+        return boardDTO;
+    }
+}
